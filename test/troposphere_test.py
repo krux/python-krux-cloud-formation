ca84756ebad001e26b91790a76fbb8f4225b574e
Peter Han <phan@krux.com>
2016-05-20 18:01:00 -0700

    Troposphere unittest

--- test/troposphere_test.py
+++ test/troposphere_test.py
@@ -8,13 +8,16 @@
 #
 
 from __future__ import absolute_import
+from copy import deepcopy
 import unittest
 
 #
 # Third party libraries
 #
 
+import simplejson
 from mock import MagicMock, patch
+from botocore.exceptions import ClientError
 
 #
 # Internal libraries
@@ -25,8 +28,20 @@ import krux_troposphere.troposphere
 
 
 class TroposphereTest(unittest.TestCase):
-    TEST_REGION = 'us-west-2'
-    TEST_QUEUE_NAME = 'jib-test'
+    TEST_STACK_NAME = 'test_stack'
+    FAKE_ERROR_RESP = {
+        'ResponseMetadata': {
+            'HTTPStatusCode': 400,
+            'RequestId': 'fake-request',
+        },
+        'Error': {
+            'Message': '',
+            'Code': 'ValidationError',
+            'Type': 'Sender',
+        }
+    }
+    STACK_NOT_EXIST_ERROR_MSG = 'Stack with id {stack_name} does not exist'.format(stack_name=TEST_STACK_NAME)
+    NO_UPDATE_ERROR_MSG = 'No updates are to be performed.'
 
     def setUp(self):
         self._cf = MagicMock()
@@ -41,5 +56,88 @@ class TroposphereTest(unittest.TestCase):
                 boto=boto
             )
 
-    def test_is_stack_exists(self):
-        pass
+    def test_is_stack_exists_success(self):
+        self._cf.get_template.return_value = True
+
+        self.assertTrue(self._troposphere._is_stack_exists(self.TEST_STACK_NAME))
+        self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
+
+    def test_is_stack_exists_failure(self):
+        resp = deepcopy(self.FAKE_ERROR_RESP)
+        resp['Error']['Message'] = self.STACK_NOT_EXIST_ERROR_MSG
+        self._cf.get_template.side_effect = ClientError(resp, '')
+
+        self.assertFalse(self._troposphere._is_stack_exists(self.TEST_STACK_NAME))
+        self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
+
+    def test_is_stack_exists_boto_error(self):
+        resp = deepcopy(self.FAKE_ERROR_RESP)
+        resp['Error']['Message'] = 'An error that I cannot handle happened'
+        self._cf.get_template.side_effect = ClientError(resp, '')
+
+        with self.assertRaises(ClientError):
+            self._troposphere._is_stack_exists(self.TEST_STACK_NAME)
+            self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
+
+    def test_is_stack_exists_std_error(self):
+        self._cf.get_template.side_effect = StandardError()
+
+        with self.assertRaises(StandardError):
+            self._troposphere._is_stack_exists(self.TEST_STACK_NAME)
+            self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
+
+    def test_save_create(self):
+        self._cf.create_stack.return_value = True
+
+        with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=False)):
+            self._troposphere.save(self.TEST_STACK_NAME)
+            self._cf.create_stack.assert_called_once_with(
+                StackName=self.TEST_STACK_NAME,
+                TemplateBody=self._troposphere.template.to_json()
+            )
+
+    def test_save_update_success(self):
+        self._cf.update_stack.return_value = True
+
+        with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=True)):
+            self._troposphere.save(self.TEST_STACK_NAME)
+            self._cf.update_stack.assert_called_once_with(
+                StackName=self.TEST_STACK_NAME,
+                TemplateBody=self._troposphere.template.to_json()
+            )
+
+    def test_save_update_no_update(self):
+        resp = deepcopy(self.FAKE_ERROR_RESP)
+        resp['Error']['Message'] = self.NO_UPDATE_ERROR_MSG
+        self._cf.update_stack.side_effect = ClientError(resp, '')
+
+        with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=True)):
+            self._troposphere.save(self.TEST_STACK_NAME)
+            self._cf.update_stack.assert_called_once_with(
+                StackName=self.TEST_STACK_NAME,
+                TemplateBody=self._troposphere.template.to_json()
+            )
+
+    def test_save_update_boto_error(self):
+        resp = deepcopy(self.FAKE_ERROR_RESP)
+        resp['Error']['Message'] = 'An error that I cannot handle happened'
+        self._cf.update_stack.side_effect = ClientError(resp, '')
+
+        with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=True)):
+            with self.assertRaises(ClientError):
+                self._troposphere.save(self.TEST_STACK_NAME)
+                self._cf.update_stack.assert_called_once_with(
+                    StackName=self.TEST_STACK_NAME,
+                    TemplateBody=self._troposphere.template.to_json()
+                )
+
+    def test_save_update_std_error(self):
+        self._cf.update_stack.side_effect = StandardError()
+
+        with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=True)):
+            with self.assertRaises(StandardError):
+                self._troposphere.save(self.TEST_STACK_NAME)
+                self._cf.update_stack.assert_called_once_with(
+                    StackName=self.TEST_STACK_NAME,
+                    TemplateBody=self._troposphere.template.to_json()
+                )
