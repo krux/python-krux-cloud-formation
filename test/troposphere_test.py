1c7f0461e1816073c879f8ddeb72ba5ba0c92990
Peter Han <phan@krux.com>
2016-05-23 15:30:51 -0700

    Docstring for unit test

--- test/troposphere_test.py
+++ test/troposphere_test.py
@@ -44,6 +44,7 @@ class TroposphereTest(unittest.TestCase):
     NO_UPDATE_ERROR_MSG = 'No updates are to be performed.'
 
     def setUp(self):
+        # Set up a fake Boto3 object
         self._cf = MagicMock()
         boto = MagicMock(
             client=MagicMock(
@@ -51,18 +52,25 @@ class TroposphereTest(unittest.TestCase):
             )
         )
 
+        # Mock isinstance() so that it will accept MagicMock object
         with patch('krux_troposphere.troposphere.isinstance', MagicMock(return_value=True)):
             self._troposphere = krux_troposphere.troposphere.Troposphere(
                 boto=boto
             )
 
     def test_is_stack_exists_success(self):
+        """
+        _is_stack_exists() handles existing stacks properly
+        """
         self._cf.get_template.return_value = True
 
         self.assertTrue(self._troposphere._is_stack_exists(self.TEST_STACK_NAME))
         self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
 
     def test_is_stack_exists_failure(self):
+        """
+        _is_stack_exists() handles non-existing stacks properly
+        """
         resp = deepcopy(self.FAKE_ERROR_RESP)
         resp['Error']['Message'] = self.STACK_NOT_EXIST_ERROR_MSG
         self._cf.get_template.side_effect = ClientError(resp, '')
@@ -71,6 +79,9 @@ class TroposphereTest(unittest.TestCase):
         self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
 
     def test_is_stack_exists_boto_error(self):
+        """
+        _is_stack_exists() handles unknown boto errors properly
+        """
         resp = deepcopy(self.FAKE_ERROR_RESP)
         resp['Error']['Message'] = 'An error that I cannot handle happened'
         self._cf.get_template.side_effect = ClientError(resp, '')
@@ -80,6 +91,9 @@ class TroposphereTest(unittest.TestCase):
             self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
 
     def test_is_stack_exists_std_error(self):
+        """
+        _is_stack_exists() handles unknown standard errors properly
+        """
         self._cf.get_template.side_effect = StandardError()
 
         with self.assertRaises(StandardError):
@@ -87,6 +101,9 @@ class TroposphereTest(unittest.TestCase):
             self._cf.get_template.assert_called_once_with(StackName=self.TEST_STACK_NAME)
 
     def test_save_create(self):
+        """
+        save() detects a new stack and creates it properly
+        """
         self._cf.create_stack.return_value = True
 
         with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=False)):
@@ -97,6 +114,9 @@ class TroposphereTest(unittest.TestCase):
             )
 
     def test_save_update_success(self):
+        """
+        save() detects an existing stack and updates it properly
+        """
         self._cf.update_stack.return_value = True
 
         with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=True)):
@@ -107,6 +127,9 @@ class TroposphereTest(unittest.TestCase):
             )
 
     def test_save_update_no_update(self):
+        """
+        save() detects when there is no change in the update and does not propagate the error
+        """
         resp = deepcopy(self.FAKE_ERROR_RESP)
         resp['Error']['Message'] = self.NO_UPDATE_ERROR_MSG
         self._cf.update_stack.side_effect = ClientError(resp, '')
@@ -119,6 +142,9 @@ class TroposphereTest(unittest.TestCase):
             )
 
     def test_save_update_boto_error(self):
+        """
+        save() handles unknown boto errors properly
+        """
         resp = deepcopy(self.FAKE_ERROR_RESP)
         resp['Error']['Message'] = 'An error that I cannot handle happened'
         self._cf.update_stack.side_effect = ClientError(resp, '')
@@ -132,6 +158,9 @@ class TroposphereTest(unittest.TestCase):
                 )
 
     def test_save_update_std_error(self):
+        """
+        save() handles unknown standard errors properly
+        """
         self._cf.update_stack.side_effect = StandardError()
 
         with patch('krux_troposphere.troposphere.Troposphere._is_stack_exists', MagicMock(return_value=True)):
